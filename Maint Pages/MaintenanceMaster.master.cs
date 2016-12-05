019fc8803600d4cb9b3bb43b2672f8fca11d1363
tclapp <timjclapp@gmail.com>
2016-12-05 13:02:26 -0600

    Add files via upload

---
+++ Maint Pages/MaintenanceMaster.master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Images_MaintenanceMaster : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("Cart.aspx");
+    }
+}
